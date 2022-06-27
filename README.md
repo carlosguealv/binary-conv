d3bd6700e9391084ef36a1152c8a471de5e46b80
Carlos Guerrero Alvarez <carlos.guerrero.alvarez.dartmouth.25@dartmouth.edu>
2022-06-27 16:00:02 -0400

    Finished program

---
+++ README.md
@@ -0,0 +1,16 @@
+# Binary-conv
+This program is a simple number converter: it changes a number from decimal to
+binary and viceversa, depending on the user input.
+
+To install the program, run:
+```bash
+cargo install --path .
+```
+
+And to run it, execute one of the following commands:
+```bash
+cargo run
+binary-conv
+```
+
+The latter command requires the program to have been installed already.
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 // mods
 mod nums;
-mod args;
 
 // imports
 use std::io;
-use clap::Parser;
-use args::Arguments;
+use std::process;
 
 fn main() {
     println!("Input a number to convert...");
@@ -21,7 +19,24 @@ fn main() {
         .parse()
         .expect("Error: input is not an integer");
 
-    let res = nums::to_decimal(num);
+    println!("Do you want to convert to b (binary) or d (decimal)?");
+
+    let mut option = String::new();
+
+    io::stdin()
+        .read_line(&mut option)
+        .expect("Error: failed to read line");
+
+    // create a variable to store the result of the conversion
+    let res: i32;
+    if option.trim() == "d" {
+        res = nums::to_decimal(num);
+    } else if option.trim() == "b" {
+        res = nums::to_binary(num);
+    } else {
+        println!("Error: the inputted option is not valid!");
+        process::exit(1);
+    }
 
     println!("The result is {}", res);
 }
